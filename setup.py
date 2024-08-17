94d09b26fbbdf0d6648a3985c8bdb9eb36923fdb
Abhishek Upadhyay <upadhyayabhishekkumar45@gmail.com>
2024-08-17 09:21:41 +0530

    setup.py

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup,find_packages
+from typing import List 
+
+HYPHEN_E_DOT='-e .'
+def get_requirements(file_path:str)->List[str]:
+    requirements = []
+    
+    with open(file_path, 'r') as file_obj:
+        requirements=file_obj.readlines()
+        requirements=[req.replace('\n','') for req in requirements]
+    
+        if HYPHEN_E_DOT in requirements:
+            requirements.remove(HYPHEN_E_DOT)
+
+        return requirements
+
+
+setup(
+    name='ConsignmentPricing',
+    version='0.0.1',
+    author='ABHISHEK UPADHYAY',
+    author_email='abhishekupadhyay9336@gmail.com',
+    install_requires=get_requirements('requirements.txt'),
+    packages=find_packages()
+)
